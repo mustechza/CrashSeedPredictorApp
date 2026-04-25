658d4b96af874884b297b06388c37c1eaef8b988
mustechza <141557155+mustechza@users.noreply.github.com>
2026-04-25 20:23:23 +0200

    Update loader.py

--- data/loader.py
+++ data/loader.py
@@ -1,9 +1,19 @@
 import json
 import pandas as pd
 
-def load_data(file=None):
-    if file is not None:
-        data = json.load(file)
-        return pd.DataFrame(data)
-    else:
-        return pd.DataFrame()  # empty fallback
+def load_data(file):
+    data = json.load(file)
+
+    df = pd.DataFrame(data)
+
+    # Normalize columns
+    df["crash"] = df["rate"].astype(float)
+
+    # Convert timestamps
+    df["prepareTime"] = pd.to_datetime(df["prepareTime"], unit="ms")
+    df["beginTime"] = pd.to_datetime(df["beginTime"], unit="ms")
+    df["endTime"] = pd.to_datetime(df["endTime"], unit="ms")
+
+    df["fetchedAt"] = pd.to_datetime(df["fetchedAt"])
+
+    return df
