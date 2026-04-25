ab858a420ca7178c6ef6c421f7e356d380e00f85
mustechza <141557155+mustechza@users.noreply.github.com>
2026-04-25 20:24:26 +0200

    Update trainer.py

--- training/trainer.py
+++ training/trainer.py
@@ -1,6 +1,16 @@
-from sklearn.model_selection import train_test_split
-
 def prepare_data(df):
-    X = df[["rolling_mean", "rolling_std"]]
+    features = [
+        "rolling_mean",
+        "rolling_std",
+        "round_duration",
+        "prep_gap",
+        "delta"
+    ]
+
+    X = df[features]
+
+    # Target: hit 1.5x
     y = (df["crash"] >= 1.5).astype(int)
+
+    from sklearn.model_selection import train_test_split
     return train_test_split(X, y, test_size=0.2, shuffle=False)
