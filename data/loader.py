7d3316959c8b01038021ac4ba6a9c06961ea231f
mustechza <141557155+mustechza@users.noreply.github.com>
2026-04-25 20:33:54 +0200

    Update loader.py

--- data/loader.py
+++ data/loader.py
@@ -3,17 +3,15 @@ import pandas as pd
 
 def load_data(file):
     data = json.load(file)
-
     df = pd.DataFrame(data)
 
-    # Normalize columns
+    # Normalize
     df["crash"] = df["rate"].astype(float)
 
     # Convert timestamps
-    df["prepareTime"] = pd.to_datetime(df["prepareTime"], unit="ms")
-    df["beginTime"] = pd.to_datetime(df["beginTime"], unit="ms")
-    df["endTime"] = pd.to_datetime(df["endTime"], unit="ms")
-
-    df["fetchedAt"] = pd.to_datetime(df["fetchedAt"])
+    df["prepareTime"] = pd.to_datetime(df["prepareTime"], unit="ms", errors="coerce")
+    df["beginTime"] = pd.to_datetime(df["beginTime"], unit="ms", errors="coerce")
+    df["endTime"] = pd.to_datetime(df["endTime"], unit="ms", errors="coerce")
+    df["fetchedAt"] = pd.to_datetime(df["fetchedAt"], errors="coerce")
 
     return df
