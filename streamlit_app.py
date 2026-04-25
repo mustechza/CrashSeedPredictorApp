fd718f5fe48a742858552b35fdd75ec8766c5e9f
mustechza <141557155+mustechza@users.noreply.github.com>
2026-04-25 20:19:57 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,10 @@
 import streamlit as st
 
-from data.loader import load_data
 from data.cleaner import clean_data
 from training.trainer import prepare_data
 from training.backtest import run_backtest
+from data.loader import load_data
+
 
 from models.random_forest import RFModel
 from analytics.dashboard import plot_equity
@@ -13,7 +14,11 @@ st.set_page_config(layout="wide")
 st.title("🚀 Crash AI Trading Dashboard")
 
 # Load data
-df = load_data()
+uploaded_file = st.sidebar.file_uploader("Upload JSON", type=["json"])
+
+if uploaded_file:
+    df = load_data(uploaded_file)
+    st.session_state.df = df
 df = clean_data(df)
 
 st.subheader("📊 Raw Data")
